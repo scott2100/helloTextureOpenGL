598a71a9938c9704a9c98a47b9470e0201cf8a01
Scott Young <wizardfatty@gmail.com>
2020-10-21 20:47:45 -0400

    Added code to flip image when loading. Added code to mix two textures.

--- main.go
+++ main.go
@@ -15,11 +15,24 @@ import (
 
 var vertices = []float32{
 	// positions       // colors        // texture coords
-	0.5,  0.5, 0.0,    1.0, 0.0, 0.0,   1.0, 1.0,   // top right
-	0.5, -0.5, 0.0,    0.0, 1.0, 0.0,   1.0, 0.0,   // bottom right
-	-0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0,   // bottom let
-	-0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0,   // top let
+	0.5,  0.5, 0.0,    1.0, 0.0, 0.0,   2, 2,   // top right
+	0.5, -0.5, 0.0,    0.0, 1.0, 0.0,   2, 0,   // bottom right
+	-0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0, 0,   // bottom let
+	-0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0, 2,   // top let
 }
+
+var triangles = []uint32 {
+	0, 1, 3, // first triangle
+	1, 2, 3,  // second triangle
+}
+
+const texture1Path = "bricks.jpg"
+//var texture2Path = "Badger.jpg"
+const texture2Path = "vines.png"
+//var texture1Path = "container.jpg"
+//var texture2Path = "awesomeface.png"
+
+var mixValue float32 = 0
 
 func main() {
 
@@ -32,7 +45,7 @@ func main() {
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, 1)
 
-	window, err := glfw.CreateWindow(640, 480, "Scott Window", nil, nil)
+	window, err := glfw.CreateWindow(1024, 768, "Scott Window", nil, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -46,54 +59,29 @@ func main() {
 	fragmentShaderPath := "./shader/shaders/transformShader.fs"
 	shader := shader.New(vertexShaderPath, fragmentShaderPath)
 
-	vao2 := makeVao(vertices)
+	vao2 := makeVao(vertices, triangles)
 
-	imgFile, err := os.Open("bricks.png")
-	if err != nil {
-		log.Fatal("os.Open: ", err)
-	}
-	defer imgFile.Close()
+	rgba1 := loadImage(texture1Path)
+	textureWrap1 := gl.CLAMP_TO_EDGE
+	texture1:= initTexture(rgba1, textureWrap1)
 
-	img, _, err := image.Decode(imgFile)
-	if err != nil {
-		log.Fatal("image.Decode: ", err)
-	}
-
-	rgba := image.NewRGBA(img.Bounds())
-	if rgba.Stride != rgba.Rect.Size().X*4 {
-		log.Fatal("image.NewRGBA: ", err)
-	}
-	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
-
-	// load and create a texture
-	// -------------------------
-	var texture uint32
-	gl.GenTextures(1, &texture)
-	gl.BindTexture(gl.TEXTURE_2D, texture) // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
-	// set the texture wrapping parameters
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)	// set texture wrapping to GL_REPEAT (default wrapping method)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
-	// set texture filtering parameters
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-
-	gl.TexImage2D(
-		gl.TEXTURE_2D,
-		0,
-		gl.RGBA,
-		int32(rgba.Rect.Size().X),
-		int32(rgba.Rect.Size().Y),
-		0,
-		gl.RGBA,
-		gl.UNSIGNED_BYTE,
-		gl.Ptr(rgba.Pix))
-	gl.GenerateMipmap(gl.TEXTURE_2D)
+	rgba2 := loadImage(texture2Path)
+	textureWrap2 := gl.REPEAT
+	texture2:= initTexture(rgba2, textureWrap2)
 
 	prog := gl.CreateProgram()
 	gl.AttachShader(prog, shader.VertexShaderCompiled)
 	gl.AttachShader(prog, shader.FragmentShaderCompiled)
 	gl.LinkProgram(prog)
-
+	gl.UseProgram(prog)
+	gl.Uniform1i(gl.GetUniformLocation(prog, gl.Str("texture1\x00")), int32(0))
+	if e := gl.GetError(); e != 0 {
+		log.Fatalf("ERROR: %X", e)
+	}
+	gl.Uniform1i(gl.GetUniformLocation(prog, gl.Str("texture2\x00")), int32(1))
+	if e := gl.GetError(); e != 0 {
+		log.Fatalf("ERROR: %X", e)
+	}
 	//render loop
 	for !window.ShouldClose() {
 		//process commands
@@ -103,32 +91,91 @@ func main() {
 		gl.ClearColor(.2, .3, .3, 1.0)
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-		//gl.UseProgram(prog)
-		//gl.BindVertexArray(vao2)
-		//
+		gl.ActiveTexture(gl.TEXTURE0)
+		if e := gl.GetError(); e != 0 {
+			log.Fatalf("ERROR: %X", e)
+		}
+		gl.BindTexture(gl.TEXTURE_2D, texture1)
+		if e := gl.GetError(); e != 0 {
+			log.Fatalf("ERROR: %X", e)
+		}
 
-		gl.BindTexture(gl.TEXTURE_2D, texture)
+		gl.ActiveTexture(gl.TEXTURE1)
+		if e := gl.GetError(); e != 0 {
+			log.Fatalf("ERROR: %X", e)
+		}
+		gl.BindTexture(gl.TEXTURE_2D, texture2)
+		if e := gl.GetError(); e != 0 {
+			log.Fatalf("ERROR: %X", e)
+		}
 
 		// render container
 		gl.UseProgram(prog)
+
+		mixLocation := gl.GetUniformLocation(prog, gl.Str("mixValue\x00"))
+		gl.UseProgram(prog)
+		gl.Uniform1f(mixLocation, mixValue)
+
 		gl.BindVertexArray(vao2)
-		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+		//gl.DrawArrays(gl.TRIANGLES, 0, 3) //draw 1 triangle
 		gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, gl.PtrOffset(0))
 
 		glfw.PollEvents()
 		window.SwapBuffers()
-
 	}
 	glfw.Terminate()
+}
+
+func initTexture(rgba *image.RGBA, wrap int) uint32 {
+	var texture uint32
+	// load and create a texture
+	// -------------------------
+	gl.GenTextures(1, &texture)
+	gl.BindTexture(gl.TEXTURE_2D, texture) // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+	// set the texture wrapping parameters
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, int32(wrap)) // set texture wrapping to GL_REPEAT (default wrapping method)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, int32(wrap))
+	// set texture filtering parameters
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+
+	// flip image: first pixel is lower left corner
+	imgWidth, imgHeight := rgba.Rect.Bounds().Dx(), rgba.Rect.Bounds().Dy()
+	data := make([]byte, imgWidth*imgHeight*4)
+	lineLen := imgWidth * 4
+	dest := len(data) - lineLen
+	for src := 0; src < len(rgba.Pix); src += rgba.Stride {
+		copy(data[dest:dest+lineLen], rgba.Pix[src:src+rgba.Stride])
+		dest -= lineLen
+	}
+
+	gl.TexImage2D(
+		gl.TEXTURE_2D,
+		0,
+		gl.RGBA,
+		int32(rgba.Rect.Bounds().Dx()),
+		int32(rgba.Rect.Bounds().Dy()),
+		0,
+		gl.RGBA,
+		gl.UNSIGNED_BYTE,
+		gl.Ptr(rgba.Pix))
+	gl.GenerateMipmap(gl.TEXTURE_2D)
+
+	return texture
 }
 
 func processInput(window *glfw.Window) {
 	if window.GetKey(glfw.KeyEscape) == glfw.Press {
 		window.SetShouldClose(true)
 	}
+	if window.GetKey(glfw.KeyUp) == glfw.Press{
+		mixValue = mixValue + 0.1
+	}
+	if window.GetKey(glfw.KeyDown) == glfw.Press{
+		mixValue = mixValue - 0.1
+	}
 }
 
-// initOpenGL initializes OpenGL
 func initOpenGL() {
 	var nrAttributes int32
 	if err := gl.Init(); err != nil {
@@ -142,27 +189,27 @@ func initOpenGL() {
 }
 
 // makeVao initializes and returns a vertex array from the points provided.
-func makeVao(points []float32) uint32 {
+func makeVao(points []float32, triangles []uint32) uint32 {
 	var vbo uint32
+	var ebo uint32
 	var vao uint32
-	var stride int32
-
-	//points only 9
-	//points and colors 18
-	//points color textures 32
-	stride = int32(4 * len(points) / 3)
-	println("stride: ", stride)
 
 	gl.GenVertexArrays(1, &vao)
+	gl.GenBuffers(1, &ebo)
 	gl.GenBuffers(1, &vbo)
+
 	gl.BindVertexArray(vao)
 
 	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
 	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
 
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, 4*len(triangles), gl.Ptr(triangles), gl.STATIC_DRAW)
+
+	// vertex coord attribute
 	gl.EnableVertexAttribArray(0)
 	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 32, gl.PtrOffset(0))
-	log.Println("In if")
+	// color coord attribute
 	gl.EnableVertexAttribArray(1)
 	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 32, gl.PtrOffset(3*4))
 	// texture coord attribute
@@ -170,4 +217,26 @@ func makeVao(points []float32) uint32 {
 	gl.EnableVertexAttribArray(2)
 
 	return vao
+}
+
+func loadImage(imagePath string) *image.RGBA {
+	imgFile, err := os.Open(imagePath)
+	if err != nil {
+		log.Fatal("os.Open: ", err)
+	}
+	defer imgFile.Close()
+
+	img, _, err := image.Decode(imgFile)
+	if err != nil {
+		log.Fatal("image.Decode: ", err)
+	}
+	rgba := image.NewRGBA(img.Bounds())
+
+	if rgba.Stride != rgba.Rect.Size().X*4 {
+		log.Fatal("image.NewRGBA: ", err)
+	}
+
+	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
+
+	return rgba
 }
--- texture/texture.go
+++ texture/texture.go
@@ -1,1 +1,6 @@
 package texture
+
+
+func main () {
+
+}
